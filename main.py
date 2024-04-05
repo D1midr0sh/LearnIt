5b5316183313a177301ba4b0d8616e8b48ac04d3
Dmitriy Kirichenko <dimidrosh@gmail.com>
2024-04-05 21:22:57 +0300

    middle of nothing

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-from flask import Flask
+from dotenv import load_dotenv
+from flask import Flask, render_template
+
+import os
+
+
+load_dotenv()
 
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "yandexlyceum_secret_key"
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "secret")
+app.config["DEBUG"] = os.environ.get("DEBUG", "No").lower() in ["true", "yes", "1"]
 
 
 def main():
@@ -11,7 +18,12 @@ def main():
 
 @app.route("/")
 def base():
-    return "Главная страница"
+    return render_template("index.html")
+
+
+@app.route("/articles")
+def articles():
+    return render_template("articles.html")
 
 
 if __name__ == "__main__":
